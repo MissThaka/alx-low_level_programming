0485b628ae2122e161eb4352c54891c4e15404f8
Thakazelwe Nyamambi <thakanyamambi@gmail.com>
2023-02-20 10:26:07 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main -entry block
+ * Description - 5-print_numbers.c
+ */
+
+int main(void)
+{
+char c = '0';
+while (c <= '9')
+{putchar(c);
+c++; }
+putchar('\n');
+return (0);
+}
