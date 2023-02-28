2b90fec1138eb7b33f6322730df256c2bff96ffe
Thakazelwe Nyamambi <thakanyamambi@gmail.com>
2023-02-28 15:37:28 -0800

    README.md

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+_strlen(char *s);
+_puts(char *s);
+print_rev(char *s);
+rev_string(char *s);
+puts2(char *str);
+puts_half(char *str);
+print_array(int *a, int n);
+*_strcpy(char *dest, char *src);
